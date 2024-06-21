21e39213d914970d2f6a66c895c1a0ba722adc83
Leo Bonato <bonato.leo@gmail.com>
2024-06-21 22:07:46 +0200

    add logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+    import logo from "$lib/logo_tomas.png"
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<svg class="absolute top-0 left-0 w-screen h-screen">
+    <image href={logo} x="50%" y="50%" width="50%" height="50%" />
+</svg>
