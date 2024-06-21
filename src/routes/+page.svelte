3d94b50d8e5240a3d06ab0c2e82c8b91b47bd884
Leo Bonato <bonato.leo@gmail.com>
2024-06-21 23:55:45 +0200

    graph

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
-    import logo from "$lib/logo_tomas.png"
+  import Graph from "./Graph.svelte";
+
+  const nodes = [
+    { id: 0, label: "logo" },
+    { id: 1, label: "photo" },
+    { id: 2, label: "video" },
+    { id: 3, label: "exhibitions" },
+    { id: 4, label: "music" },
+    { id: 5, label: "bio" },
+  ];
+
+  const links = [
+    { source: 1, target: 0, value: 1 },
+    { source: 2, target: 0, value: 1 },
+    { source: 3, target: 0, value: 1 },
+    { source: 4, target: 0, value: 1 },
+    { source: 5, target: 0, value: 1 },
+  ];
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<svg class="absolute top-0 left-0 w-screen h-screen">
-    <image href={logo} x="50%" y="50%" width="50%" height="50%" />
-</svg>
+<div class="absolute top-0 left-0 w-screen h-screen">
+  <Graph {links} {nodes} />
+</div>
---
+++ src/routes/Graph.svelte
@@ -0,0 +1,110 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import * as d3 from "d3";
+
+  interface NodeDatum extends d3.SimulationNodeDatum {
+    id: number;
+    label: string;
+  }
+  interface LinkDatum extends d3.SimulationLinkDatum<NodeDatum> {}
+
+  export let nodes: NodeDatum[];
+  export let links: LinkDatum[];
+
+  let svg: SVGSVGElement;
+  let width = 500;
+  let height = 600;
+  const nodeRadius = 45;
+
+  $: links = links.map((d) => Object.create(d));
+  $: nodes = nodes.map((d) => Object.create(d));
+
+  const colourScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+  let simulation: d3.Simulation<NodeDatum, LinkDatum>;
+
+  onMount(() => {
+    simulation = d3
+      .forceSimulation<NodeDatum, LinkDatum>(nodes)
+      .force(
+        "link",
+        d3
+          .forceLink<NodeDatum, LinkDatum>(links)
+          .id((d) => d.id)
+          .distance(100)
+      )
+      .force("charge", d3.forceManyBody().strength(-300))
+      .force("collide", d3.forceCollide(nodeRadius))
+      .on("tick", simulationUpdate);
+
+    d3.select(svg as Element).call(
+      d3
+        .drag()
+        .container(svg)
+        .subject(dragsubject)
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended)
+    );
+  });
+
+  function simulationUpdate() {
+    simulation.tick();
+    nodes = [...nodes];
+    links = [...links];
+  }
+
+  type DragEvent = d3.D3DragEvent<SVGCircleElement, NodeDatum, NodeDatum>;
+
+  function dragsubject(event: DragEvent) {
+    return simulation.find(event.x, event.y, nodeRadius);
+  }
+
+  function dragstarted(event: DragEvent) {
+    if (!event.active) simulation.alphaTarget(0.3).restart();
+    event.subject.fx = event.subject.x;
+    event.subject.fy = event.subject.y;
+  }
+
+  function dragged(event: DragEvent) {
+    event.subject.fx = event.x;
+    event.subject.fy = event.y;
+  }
+
+  function dragended(event: DragEvent) {
+    if (!event.active) simulation.alphaTarget(0);
+    event.subject.fx = null;
+    event.subject.fy = null;
+  }
+</script>
+
+<svelte:window bind:innerWidth={width} bind:innerHeight={height} />
+
+<svg
+  bind:this={svg}
+  {width}
+  {height}
+  viewBox="{-(width / 2) + 100} {-height / 2} {width} {height}"
+>
+  <g id="graph">
+    <!-- {#each links as link}
+      <g stroke="#999" stroke-opacity="0.6">
+        <line
+          x1={(link.source as NodeDatum).x}
+          y1={(link.source as NodeDatum).y}
+          x2={(link.target as NodeDatum).x}
+          y2={(link.target as NodeDatum).y}
+        />
+    {/each} -->
+
+    {#each nodes as node}
+      <circle
+        class="node"
+        r={nodeRadius}
+        fill={colourScale(node.id.toString())}
+        cx={node.x}
+        cy={node.y}
+      />
+    {/each}
+  </g></svg
+>
