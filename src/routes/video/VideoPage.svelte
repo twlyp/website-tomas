408958035a5e3d2c549cf6c43a33ef8b6434c30c
Leo Bonato <bonato.leo@gmail.com>
2024-07-04 22:53:27 +0200

    add more videos

--- src/routes/video/VideoPage.svelte
+++ src/routes/video/VideoPage.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
   import VideoCard from "./VideoCard.svelte";
 
-  const videos = [{ title: "Transitions - nowness", videoId: "6VCX670GnpU" }];
+  const videos = [
+    { title: "Transitions - nowness", videoId: "6VCX670GnpU" },
+    { title: "Forgive You Not - Tyler Matthew Oyer", videoId: "7Qmv9T8pZKo" },
+    { title: "Starving Vulture - Soya the Cow ", videoId: "pUsZGJKp-b8" },
+    { title: "M U I O P O T M O S: the fate of the butterfly - Infringe", videoId: "UgVQQsdqGJU" },
+  ];
 </script>
 
 <div>
-    {#each videos as { title, videoId }}
-        <VideoCard {title} {videoId} />
-    {/each}
+  {#each videos as { title, videoId }}
+    <VideoCard {title} {videoId} />
+  {/each}
 </div>
