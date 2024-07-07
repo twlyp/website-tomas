3b00f40ef406f879ef8dbf690754fbc0a91aefef
Leo Bonato <bonato.leo@gmail.com>
2024-07-07 16:50:29 +0200

    add swipe to carousel

--- src/components/Carousel/Carousel.svelte
+++ src/components/Carousel/Carousel.svelte
@@ -1,39 +1,50 @@
 <script lang="ts">
-import type { ComponentProps } from "svelte";
-// biome-ignore lint/style/useImportType: it's used as component
-import PhotoCard from "../../routes/photo/PhotoCard.svelte";
+  import type { ComponentProps } from "svelte";
+  // biome-ignore lint/style/useImportType: it's used as component
+  import PhotoCard from "../../routes/photo/PhotoCard.svelte";
+  import { swipe, type SwipeCustomEvent } from "svelte-gestures";
 
-export let photos: Array<ComponentProps<PhotoCard>>;
-let currentPhotoIdx = Math.floor(Math.random() * photos.length);
+  export let photos: Array<ComponentProps<PhotoCard>>;
+  let currentPhotoIdx = Math.floor(Math.random() * photos.length);
 
-function mod(n: number, m: number) {
-	return ((n % m) + m) % m;
-}
+  function mod(n: number, m: number) {
+    return ((n % m) + m) % m;
+  }
 
-function changePhoto(delta: number) {
-	currentPhotoIdx = mod(currentPhotoIdx + delta, photos.length);
-}
+  function changePhoto(delta: number) {
+    currentPhotoIdx = mod(currentPhotoIdx + delta, photos.length);
+  }
 
-function onKeydown(ev: KeyboardEvent) {
-	if (ev.key === "ArrowRight" || ev.key === "ArrowDown") {
-		changePhoto(+1);
-	} else if (ev.key === "ArrowLeft" || ev.key === "ArrowUp") {
-		changePhoto(-1);
-	}
-}
+  function onKeydown(ev: KeyboardEvent) {
+    if (ev.key === "ArrowRight" || ev.key === "ArrowDown") {
+      changePhoto(+1);
+    } else if (ev.key === "ArrowLeft" || ev.key === "ArrowUp") {
+      changePhoto(-1);
+    }
+  }
 
-function focus(element: HTMLElement) {
-	element.focus();
-}
+  function onSwipe(ev: SwipeCustomEvent) {
+    if (ev.detail.direction === "left") {
+      changePhoto(+1);
+    } else if (ev.detail.direction === "right") {
+      changePhoto(-1);
+    }
+  }
+
+  function focus(element: HTMLElement) {
+    element.focus();
+  }
 </script>
 
 <div
   class="w-full h-full"
+  use:focus
+  use:swipe
   on:click|stopPropagation={() => changePhoto(+1)}
   on:keydown={onKeydown}
+  on:swipe={onSwipe}
   role="button"
   tabindex="0"
-  use:focus
 >
   {#each photos as photo, idx}
     <PhotoCard
