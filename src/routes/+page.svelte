ffeb2ddb49ff85fc809a642a1ec6713ae9af63ea
Leo Bonato <bonato.leo@gmail.com>
2024-07-07 15:17:07 +0200

    use stores for viewport width/height

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,15 @@
   import Graph from "./Graph.svelte";
   import OpenPage from "./OpenPage.svelte";
   import { NODES } from "./constants";
+  import {viewportWidth, viewportHeight} from './stores';
 
-  let viewportWidth: number;
-  let viewportHeight: number;
 </script>
 
-<svelte:window bind:innerWidth={viewportWidth} bind:innerHeight={viewportHeight} />
+<svelte:window bind:innerWidth={$viewportWidth} bind:innerHeight={$viewportHeight} />
 
 <div class="absolute top-0 left-0 w-screen h-screen">
-  {#if viewportWidth && viewportHeight}
-    <Graph width={viewportWidth} height={viewportHeight} nodes={NODES} />
+  {#if $viewportWidth && $viewportHeight}
+    <Graph width={$viewportWidth} height={$viewportHeight} nodes={NODES} />
   {/if}
 </div>
 
--- src/routes/stores.ts
+++ src/routes/stores.ts
@@ -2,3 +2,6 @@ import { writable } from "svelte/store";
 import type { PAGES } from "./constants";
 
 export const currentPage = writable<PAGES | null>(null);
+
+export const viewportWidth = writable<number>();
+export const viewportHeight = writable<number>();
