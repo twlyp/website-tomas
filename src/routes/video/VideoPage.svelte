fe53db71c3d12637cff99750425a9af7fd7d2950
Leo Bonato <bonato.leo@gmail.com>
2025-03-11 18:18:26 +0100

    rm VideoPage scrolling

--- src/routes/video/VideoPage.svelte
+++ src/routes/video/VideoPage.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-  import { onMount } from "svelte";
   import VideoCard from "./VideoCard.svelte";
-  import { bindToInterval } from "../utils";
 
   let width: number;
   let height: number;
@@ -15,40 +13,14 @@
       videoId: "UgVQQsdqGJU",
     },
   ];
-
-  let container: HTMLDivElement;
-
-  function scrollTo(y: number) {
-    container.scrollTo({ top: y, behavior: "smooth" });
-  }
-
-  let targetScrollTop = 0;
-  function onWheel(ev: WheelEvent) {
-    if (Math.abs(container.scrollTop - targetScrollTop) > 1 && ev.deltaY < 20) return;
-
-    targetScrollTop = bindToInterval(
-      0,
-      container.scrollHeight - height,
-      container.scrollTop + Math.sign(ev.deltaY) * height
-    );
-    scrollTo(targetScrollTop);
-  }
-
-  onMount(() => {
-    scrollTo(0);
-  });
 </script>
 
 <div
   class="relative w-full h-full overflow-scroll no-scrollbar"
   bind:clientWidth={width}
   bind:clientHeight={height}
-  bind:this={container}
 >
   {#each videos as { title, videoId }}
     <VideoCard {title} {videoId} parentWidth={width} parentHeight={height} />
   {/each}
 </div>
-
-<div class="absolute top-0 left-0 w-full h-full" on:wheel|capture={onWheel} />
-
