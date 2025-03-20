81f7c7fb3b5d6c04e2ce09cfbf1e8b34c304a6b6
Leo Bonato <bonato.leo@gmail.com>
2025-03-20 19:18:27 +0100

    use new font for coming soon

--- src/routes/exhibitions/+page.svelte
+++ src/routes/exhibitions/+page.svelte
@@ -5,6 +5,6 @@
 
 <PageSection page={PAGES.EXHIBITIONS}>
   <div class="flex h-full w-full flex-col items-center justify-center text-amber-50">
-    <p class="text-lg">Coming soon...</p>
+    <p class="text-lg font-tomeyza">coming soon...</p>
   </div>
 </PageSection>
