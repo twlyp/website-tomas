74f4c490b8015b8dfceafd803b64a2fc06219c2e
Leo Bonato <bonato.leo@gmail.com>
2025-03-20 18:11:47 +0100

    switch to prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,29 +2,32 @@ import js from "@eslint/js";
 import ts from "typescript-eslint";
 import svelte from "eslint-plugin-svelte";
 import globals from "globals";
+import svelteConfig from "./svelte.config.js";
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-	js.configs.recommended,
-	...ts.configs.recommended,
-	...svelte.configs["flat/recommended"],
-	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node,
-			},
-		},
-	},
-	{
-		files: ["**/*.svelte"],
-		languageOptions: {
-			parserOptions: {
-				parser: ts.parser,
-			},
-		},
-	},
-	{
-		ignores: ["build/", ".svelte-kit/", "dist/", "node_modules/"],
-	},
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...svelte.configs["flat/prettier"],
+  //   prettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
+    languageOptions: {
+      parserOptions: {
+        parser: ts.parser,
+        svelteConfig,
+      },
+    },
+  },
+  {
+    ignores: ["build/", ".svelte-kit/", "dist/", "node_modules/"],
+  },
 ];
