79d1a6f74f87a636b351d82b6a5b6c0edb331f6d
Leo Bonato <bonato.leo@gmail.com>
2025-03-22 14:47:53 +0100

    change breakpoint to lg:

--- src/routes/music/CardMusic.svelte
+++ src/routes/music/CardMusic.svelte
@@ -8,15 +8,15 @@
   const { children, title, text, id }: Props = $props();
 </script>
 
-<div class="pb-20 flex w-full flex-col items-center md:grid md:grid-cols-2" {id}>
-  <h1 class="mb-5 font-tomeyza text-3xl md:hidden">{title}</h1>
+<div class="pb-20 flex w-full flex-col items-center lg:grid lg:grid-cols-2" {id}>
+  <h1 class="mb-5 font-tomeyza text-3xl lg:hidden">{title}</h1>
 
-  <div class="mb-5 justify-self-center md:mb-0">
+  <div class="mb-5 justify-self-center lg:mb-0">
     {@render children()}
   </div>
 
-  <div class="md:ml-10 md:justify-self-start">
-    <h1 class="mb-5 hidden font-tomeyza text-3xl md:block">{title}</h1>
+  <div class="lg:ml-10 lg:justify-self-start">
+    <h1 class="mb-5 hidden font-tomeyza text-3xl lg:block">{title}</h1>
     {#if text}
       <p class="font-tomasito text-2xl">{text}</p>
     {/if}
