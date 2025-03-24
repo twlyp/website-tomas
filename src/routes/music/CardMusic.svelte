2f9d70e8535128003861624e4de0c196f6b209ea
Leo Bonato <bonato.leo@gmail.com>
2025-03-24 19:39:25 +0100

    fix styling of CardMusic

--- src/routes/music/CardMusic.svelte
+++ src/routes/music/CardMusic.svelte
@@ -8,14 +8,14 @@
   const { class: klass, children, style, title, text}: Props = $props();
 </script>
 
-<div class="flex h-full w-full flex-col items-center lg:grid lg:grid-cols-2 {klass}" {style}>
-  <h1 class="mb-5 font-tomeyza text-3xl lg:hidden">{title}</h1>
+<div class="flex h-full w-full flex-col items-center justify-center lg:grid lg:grid-cols-2 {klass}" {style}>
+  <h1 class="mb-5 font-tomeyza text-3xl text-center lg:hidden">{title}</h1>
 
   <div class="mb-5 justify-self-center lg:mb-0">
       {@render children()}
   </div>
 
-  <div class="lg:ml-10 lg:justify-self-start">
+  <div class="lg:ml-10 lg:justify-self-start text-center lg:text-left">
     <h1 class="mb-5 hidden font-tomeyza text-3xl lg:block">{title}</h1>
     {#if text}
       <p class="font-tomasito text-2xl">{text}</p>
