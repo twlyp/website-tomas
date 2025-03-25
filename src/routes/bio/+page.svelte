e20bf5825eda1e1f34583605195230c4c2834cf2
Leo Bonato <bonato.leo@gmail.com>
2025-03-25 15:21:56 +0100

    add bio

--- src/routes/bio/+page.svelte
+++ src/routes/bio/+page.svelte
@@ -4,7 +4,60 @@
 </script>
 
 <PageSection page={PAGES.BIO}>
-  <div class="flex h-full w-full flex-col items-center justify-center text-amber-50">
-    <p class="text-lg font-tomeyza">coming soon...</p>
+  <div
+    class="no-scrollbar flex h-full w-full flex-col items-center justify-items-center
+           overflow-scroll pt-10 text-center font-serif text-amber-50"
+  >
+    <div class="max-w-2xl">
+      <p class="mb-5">
+        Tomás Eyzaguirre is an in-disciplinary artist who explores corporeality in various
+        manifestations of art.
+      </p>
+
+      <p class="mb-5">
+        Emerging from fashion photography, his approach to imagery stems from the creation of
+        characters through costume, examining how it transforms the persona and how the body
+        structures the attire, thus completing the fantasy. His photography has expanded through a
+        process of self-discipline, addressing various areas of this art and applying the same
+        concepts, from object intervention to the creation of simple wooden installations that he
+        constructs, dresses, photographs, and then dismantles, analyzing the ultimate abstraction of
+        corporeality.
+      </p>
+
+      <p class="mb-5">
+        In music, these principles manifest in the search for the corporeality of the voice. Under
+        the persona of [Auno], born in an artist residency, Eyzaguirre explored singing inside a
+        cave to replicate his ecclesiastical past. The cave’s lack of echo led him to recreate it
+        through loudspeakers, resulting in a study of sound waves and physical phenomena that
+        defined his vocal style. Working primarily with singing, extended vocal techniques,
+        microphones, and amplification, he creates complex sounds and atmospheres filled with
+        feedback, transgressing gender conventions and revealing the ambiguous and plastic qualities
+        of the voice.
+      </p>
+
+      <p class="mb-5">
+        In drag performance, Eyzaguirre lends his body to [Soyla Tempestad], who intervenes with
+        garments found on the street under the concept of "Zu-verschenken" (to give away). Dressed
+        in this "armor," Tempestad takes over Tomeyza’s body and performs solo shows.
+      </p>
+
+      <p class="mb-5">
+        His findings emerge from residencies, collaborations, and constant experimentation in
+        artistic projects. He defines himself as an in-disciplinary artist due to his
+        non-traditional relationship with art, based on self-imposed experimentation, the authority
+        of failure, error, intuition, and feedback from his peers.
+      </p>
+
+      <p class="mb-5">
+        He has exhibited and performed in various cultural spaces, including: "Evaluación sin Costo"
+        (CL 2024), "KULE" (DE 2023-22-20), "The Ballery" (DE 2023-22-21), "Hermitage Sykaminea"
+        (GR-LV 2020-21), "MOLT" (DE 2023), "Galería MANA" (IP 2024), "Eboded Festival" (IL 2023),
+        "ICC - Berlin Questions" (DE 2022), "Dock 11" (DE 2022), "Galerie Grölman" (DE 2022), "Salon
+        Velour" (DE 2023-22), "Tipsy Bear" (DE 2023), "Paolo Pinkel" (DE 2021), "The White
+        Rectangle" (DE 2021-19), "Nativos" (CL-XX 2022), "Ergo Collective" (GR 2022), "Startbahn"
+        (DE 2022), "Monopol" (DE 2022), "Copeland Gallery" (UK 2024), "Berlin Fashion Week" (DE
+        2024), "Andes Fashion Week" (CL 2024), "New York Fashion Week" (NY 2024).
+      </p>
+    </div>
   </div>
 </PageSection>
