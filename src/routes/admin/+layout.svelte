e74900157cc870c92cd5c6fd83eb27597e528c65
leo <bonato.leo@gmail.com>
2025-09-20 01:19:08 +0200

    move auth check for admin

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,10 +1,41 @@
 <script lang="ts">
-  import { FirebaseApp } from "sveltefire";
-  import { auth, firestore, storage } from "$firebase";
+  import { FirebaseApp } from "sveltefire"
+  import { auth, firestore, storage } from "$firebase"
+  import { userStore, SignedIn, SignedOut } from "sveltefire"
+  import { signInWithPopup, GoogleAuthProvider, signOut, type ParsedToken } from "@firebase/auth"
+  import { derived } from "svelte/store"
 
-  let { children } = $props();
+  const user = userStore(auth)
+  const claims = derived<typeof user, ParsedToken | null>(user, ($user, set) => {
+    if ($user) {
+      $user.getIdTokenResult().then((result) => {
+        set(result?.claims)
+      })
+    } else {
+      set(null)
+    }
+  })
+
+  let { children } = $props()
 </script>
 
 <FirebaseApp {auth} {firestore} {storage}>
-  {@render children()}
+  <header class="flex flex-row items-center p-2">
+    <SignedOut>
+      <button
+        class="btn btn-primary"
+        onclick={() => signInWithPopup(auth, new GoogleAuthProvider())}
+      >
+        sign in with google
+      </button>
+    </SignedOut>
+
+    <SignedIn>
+      <button class="btn btn-primary" onclick={() => signOut(auth)}> sign out </button>
+    </SignedIn>
+  </header>
+
+  {#if $claims?.admin}
+    {@render children()}
+  {/if}
 </FirebaseApp>
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,36 +1,5 @@
 <script lang="ts">
-  import { userStore, SignedIn, SignedOut } from "sveltefire";
-  import { auth } from "$firebase";
-  import {
-    signInWithRedirect,
-    GoogleAuthProvider,
-    signOut,
-    type ParsedToken,
-  } from "@firebase/auth";
-  import { derived } from "svelte/store";
-  import AdminPanel from "$components/AdminPanel.svelte";
-
-  const user = userStore(auth);
-  const claims = derived<typeof user, ParsedToken>(user, ($user, set) => {
-    $user?.getIdTokenResult().then((result) => {
-      set(result?.claims);
-    });
-  });
+  import AdminPanel from "$components/AdminPanel.svelte"
 </script>
 
-<SignedOut>
-  <button
-    class="btn btn-primary"
-    onclick={() => signInWithRedirect(auth, new GoogleAuthProvider())}
-  >
-    sign in with google
-  </button>
-</SignedOut>
-
-<SignedIn>
-  <button class="btn btn-primary" onclick={() => signOut(auth)}> sign out </button>
-
-  {#if $claims?.admin}
-    <AdminPanel />
-  {/if}
-</SignedIn>
+<AdminPanel />
