4ef84b32c531cd8903a4dca85212c3f58165994e
leo <bonato.leo@gmail.com>
2025-09-21 01:57:18 +0200

    convert PAGES to const-enum

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,13 @@
 import * as d3 from "d3"
 
-export enum PAGES {
-  PHOTO = "photo",
-  VIDEO = "video",
-  EXHIBITIONS = "exhibitions",
-  MUSIC = "music",
-  BIO = "bio",
-}
+export const PAGES = {
+  PHOTO: "photo",
+  VIDEO: "video",
+  EXHIBITIONS: "exhibitions",
+  MUSIC: "music",
+  BIO: "bio",
+} as const
+export type PAGES = (typeof PAGES)[keyof typeof PAGES]
 
 const colorScale = d3.scaleOrdinal(d3.schemeCategory10)
 
