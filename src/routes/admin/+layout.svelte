3c5591e0ac46946f520b8329d5ceab54ee2a63fa
leo <bonato.leo@gmail.com>
2025-09-21 22:26:42 +0200

    rm svelte-firekit

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { FirebaseApp } from "svelte-firekit"
   import LoginLogoutButton from "$lib/components/AdminLoginLogoutButton.svelte"
   import { PAGES } from "$lib/constants"
   import { page as sveltePage } from "$app/state"
@@ -13,29 +12,27 @@
   let { children } = $props()
 </script>
 
-<FirebaseApp>
-  <header class="flex flex-row items-center p-2">
-    <nav class="navbar bg-base-100 shadow-sm">
-      <div class="navbar-start">
-        {#if firebaseUser.isAdmin}
-          <ul class="flex flex-row gap-3">
-            {#each links as { href, pageName }}
-              <li>
-                <a {href} class="link link-hover {sveltePage.route.id === href ? 'font-bold' : ''}">
-                  {pageName}
-                </a>
-              </li>
-            {/each}
-          </ul>
-        {/if}
-      </div>
-      <div class="navbar-end">
-        <LoginLogoutButton />
-      </div>
-    </nav>
-  </header>
+<header class="flex flex-row items-center p-2">
+  <nav class="navbar bg-base-100 shadow-sm">
+    <div class="navbar-start">
+      {#if firebaseUser.isAdmin}
+        <ul class="flex flex-row gap-3">
+          {#each links as { href, pageName }}
+            <li>
+              <a {href} class="link link-hover {sveltePage.route.id === href ? 'font-bold' : ''}">
+                {pageName}
+              </a>
+            </li>
+          {/each}
+        </ul>
+      {/if}
+    </div>
+    <div class="navbar-end">
+      <LoginLogoutButton />
+    </div>
+  </nav>
+</header>
 
-  {#if firebaseUser.isAdmin}
-    {@render children()}
-  {/if}
-</FirebaseApp>
+{#if firebaseUser.isAdmin}
+  {@render children()}
+{/if}
