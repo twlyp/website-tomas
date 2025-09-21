7e3399fb2c66186011bdcd667fca969dd5e7c6f8
leo <bonato.leo@gmail.com>
2025-09-21 23:28:23 +0200

    extract InputFiles

--- src/lib/components/CreatePhotoItemForm/CreatePhotoItemForm.svelte
+++ src/lib/components/CreatePhotoItemForm/CreatePhotoItemForm.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
   import { photoDb, type PhotoItem } from "$lib/db"
+  import type { ChangeEventHandler } from "svelte/elements"
   import InputText from "./InputText.svelte"
+  import InputFiles from "./InputFiles.svelte"
 
-  const defaultValues: PhotoItem = {
+  type FormValues = Omit<PhotoItem, "urls"> & { urls: string[] | null }
+
+  const defaultValues: FormValues = {
     title: "",
     publication: "",
     location: "",
@@ -11,22 +15,32 @@
     urls: [],
   }
 
-  let values: PhotoItem = $state(defaultValues)
+  let values: FormValues = $state(defaultValues)
   function reset() {
     values = { ...defaultValues }
   }
 
+  function validate(values: FormValues) {
+    if (!values.urls || values.urls.length === 0) {
+      console.error("no urls")
+    }
+    return values as PhotoItem
+  }
+
   async function onSubmit() {
-    await photoDb.add(values)
+    const item = validate(values)
+    await photoDb.add(item)
     reset()
   }
 </script>
 
-<form onsubmit={onSubmit}>
+<form onsubmit={onSubmit} class="flex max-w-xs flex-col gap-1">
   <InputText name="title" bind:value={values.title} />
   <InputText name="publication" bind:value={values.publication} />
   <InputText name="location" bind:value={values.location} />
   <InputText name="date" bind:value={values.date} />
+
+  <InputFiles bind:urls={values.urls} />
 
   <button type="submit" class="btn btn-primary">submit</button>
 </form>
---
+++ src/lib/components/CreatePhotoItemForm/InputFiles.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type { ChangeEventHandler } from "svelte/elements"
+
+  let { urls = $bindable() }: { urls: string[] | null } = $props()
+  let files = $state<FileList | null>(null)
+
+  const onChangeFiles: ChangeEventHandler<HTMLInputElement> = (ev) => {}
+</script>
+
+<input type="file" class="file-input" multiple bind:files onchange={onChangeFiles} />
