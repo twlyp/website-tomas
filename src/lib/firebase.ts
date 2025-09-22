4760b031def87b98799f695dbd467d6e4b24bcbb
leo <bonato.leo@gmail.com>
2025-09-22 22:51:09 +0200

    update env vars

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,23 +2,14 @@ import { initializeApp } from "firebase/app"
 import { connectAuthEmulator, getAuth } from "firebase/auth"
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore"
 import { connectStorageEmulator, getStorage, ref as storageRef } from "firebase/storage"
-import {
-  PUBLIC_FIREBASE_API_KEY,
-  PUBLIC_FIREBASE_APP_ID,
-  PUBLIC_FIREBASE_AUTH_DOMAIN,
-  PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  PUBLIC_FIREBASE_PROJECT_ID,
-  PUBLIC_FIREBASE_STORAGE_BUCKET,
-  PUBLIC_USE_EMULATORS,
-} from "$env/static/public"
 
 const firebaseConfig = {
-  apiKey: PUBLIC_FIREBASE_API_KEY,
-  authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: PUBLIC_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 export const firebaseApp = initializeApp(firebaseConfig)
@@ -27,7 +18,7 @@ export const auth = getAuth(firebaseApp)
 export const firestore = getFirestore(firebaseApp)
 export const storage = getStorage(firebaseApp)
 
-if (import.meta.env.DEV && PUBLIC_USE_EMULATORS === "true") {
+if (import.meta.env.DEV && import.meta.env.VITE_USE_EMULATORS === "true") {
   connectAuthEmulator(auth, "http://localhost:9099")
   connectFirestoreEmulator(firestore, "localhost", 8080)
   connectStorageEmulator(storage, "localhost", 9199)
